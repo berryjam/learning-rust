473765b84251d0aac77f3861faa4e292f05c5a24
berryjamcoding <berryjamcoding@gmail.com>
2021-11-24 09:56:20 +0800

    ignore 15 target

--- 15_smart_pointers/running_code_on_cleanup_with_the_drop_trait/target/debug/deps/running_code_on_cleanup_with_the_drop_trait-98b58501ca179242.d
+++
@@ -1,5 +0,0 @@
-/Users/huangjinkun/Documents/rust_workspace/src/github.com/berryjam/learning-rust/15_smart_pointers/running_code_on_cleanup_with_the_drop_trait/target/debug/deps/running_code_on_cleanup_with_the_drop_trait-98b58501ca179242: src/main.rs
-
-/Users/huangjinkun/Documents/rust_workspace/src/github.com/berryjam/learning-rust/15_smart_pointers/running_code_on_cleanup_with_the_drop_trait/target/debug/deps/running_code_on_cleanup_with_the_drop_trait-98b58501ca179242.d: src/main.rs
-
-src/main.rs:
--- 15_smart_pointers/using_box_to_point_to_data_on_the_heap/target/debug/using_box_to_point_to_data_on_the_heap.d
+++
@@ -1,1 +0,0 @@
-/Users/huangjinkun/Documents/rust_workspace/src/github.com/berryjam/learning-rust/15_smart_pointers/using_box_to_point_to_data_on_the_heap/target/debug/using_box_to_point_to_data_on_the_heap: /Users/huangjinkun/Documents/rust_workspace/src/github.com/berryjam/learning-rust/15_smart_pointers/using_box_to_point_to_data_on_the_heap/src/main.rs
